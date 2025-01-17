da435fbd615049693bfea2a21665df94cb0508bd
manoj7019 <124152401+manoj7019@users.noreply.github.com>
2025-01-18 00:22:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+/* const btn = document.getElementById('addVB');
+
+btn.addEventListener('click', function clickAddBtn() {
+    const initialText = 'Add +';
+
+    btn.innerHTML = `<span style='background-color: red'> 1 <span>`;
+});
+
+const btnPB = document.getElementById('addPB');
+
+btnPB.addEventListener('click', function clickAddPBBtn() {
+    const initialText = 'Add +';
+
+    
+    btnPB.innerHTML = `1`;
+}) */
